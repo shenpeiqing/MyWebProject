6f261a641c4e5663f3d5017e0df117168e868f23
master <284309510@qq.com>
2018-07-13 21:37:55 +0800

    sfsdfsd

--- MyWeb/src/org/westos/servletdemo/MyServlet.java
+++ MyWeb/src/org/westos/servletdemo/MyServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class MyServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        System.out.println("hehehehhe");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
